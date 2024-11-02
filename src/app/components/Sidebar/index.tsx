6743ede28a914b4e98386108ed5fd4e15b554f13
paulobr4z <paulobrazaraujo@gmail.com>
2024-11-02 12:31:43 -0300

    initial config

---
+++ src/app/components/Sidebar/index.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import * as S from './styles'
+
+export function Sidebar() {
+  return (
+    <S.Container>
+      <h1>Sidebar</h1>
+    </S.Container>
+  )
+}
---
+++ src/app/components/Sidebar/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: #121212;
+`
